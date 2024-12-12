4845acf4f20b7d4d5d891e0c1720d5c684f483b1
Kathy Satterlee <me@ksatter.com>
2024-12-12 10:54:03 -0600

    Add logic for passing image path without flag

--- fleet/scripts/manage_wallpaper/generate.sh
+++ fleet/scripts/manage_wallpaper/generate.sh
@@ -11,13 +11,21 @@ while getopts ":i:n:t:" arg; do
     t) # Optional override for default_dir
       target_path_arg="$OPTARG"
       ;;
-    g) # Set for all users 
-      global=true
+    g) # Set for all users - TODO
       ;;
-    u) #set for a specific user
-      target_user="$OPTARG"
+    u) # set for a specific user - TODO
+      ;;
+    o) # set desired output [script/profile/command/policy]- TODO
+      ;;
+    a) # apply to Fleet - TODO
+      ;;
   esac
+  shift $((OPTIND-1))
 done
+
+# If no image path was passed as a flag and there was an unflagged argument, assume that was 
+# the file path was passed without a flag
+if [ -z $image_path ]; then image_path=$1; fi
 
 # Parse args and set up default values
 raw_name=${image_name_arg:-$(basename $image_path)}
@@ -35,17 +43,23 @@ fi
 # Writing to file seems to require an extension for the decoded image 
 # Not sure why/if that's something I can do better, but for now I'm enforcing 
 # the extension pulled from the mime type
+
 raw_name=${raw_name%%.*}
-file_type=${file_type#*/}
 image_name=${raw_name}.${file_type}
 target_dir=${target_dir_arg:-"/opt/orbit/images"}
 generated_file="./set_${raw_name//[^a-zA-Z0-9]/_}_wallpaper"
 image_base64=$(base64 -i "$image_path")
 
+# The encoded image was too much for sed to handle. There may be a 
+# better way of going about this, but for now, just manually inserting 
+# the beginning of the file
+
 echo $image_name
 echo "#! bin/sh \n" > $generated_file
 echo "image_base64=\"$image_base64\"" >> $generated_file
 
+
+# Grab the rest of the script from the template file
 
 sed  -e "s#{{target_dir}}#$target_dir#g"  \
      -e "s#{{image_name}}#$image_name#g" \
